9b349f029f8b6c709049e95ca22b23ace41e0071
Marcos Aybar <aybarmarcos18@gmail.com>
2023-07-05 15:47:43 -0300

    creando archivos basicos

---
+++ codigo.js
@@ -0,0 +1,54 @@
+function suma(){
+    let numero1 = parseInt(prompt('Ingrese un numero'));
+    let numero2 = parseInt(prompt('mas'));
+    
+    console.log(alert(numero1 + numero2));
+}
+
+suma();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nombre = prompt("Ingrese su nombre");
+// let dia = prompt("Ingrese el día de su cumpleaños" + " (Ej: 24)");
+// let mes = prompt("Y ahora su mes" + " Ej: Marzo");
+
+// let Enero = 1;
+// let Febrero = 2
+// let Marzo = 3;
+
+// if((dia >= 21 && dia <=31) && mes == Enero){
+//     alert(fecha1 = nombre + " es de Acuario");
+
+// }else if( (dia >= 1 && dia <= 19) && mes == Febrero){
+//     alert(fecha2 = nombre + " es de Acuario");
+//     console.log(alert);
+// }
+
+// if((dia >= 20 && dia <=30) && mes == Febrero){
+//     alert(fecha2 = nombre + " es de Picis");
+//     console.log(alert);
+
+// }else if( (dia >= 1 && dia <= 20) && mes == Marzo){
+//     alert(fecha3 = nombre + " es de Picis");
+//     console.log(alert);
+// }
+
+
+
+
+
+
+
+
